feafd958bc6773d5be95f91b99b815a4e60c9e3f
iverson.matthew17 <iverson.matthew17@LCHS-LAB31.lcs.libertycommon.org>
2017-01-10 10:37:13 -0700

    Changed things

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,5 +13,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("new thing I just added");
+        System.out.println("Something i want to remove");
     }
 }
